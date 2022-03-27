bb9c44881db2f5e5b3ff21ff6321d2adf9ed8e98
Scorpion <scorpion@dgu.ac.kr>
2022-03-27 21:16:17 +0900

    feat(member): create member interface

---
+++ server/src/main/java/nostaljic/core/member/Grade.java
@@ -0,0 +1,5 @@
+package nostaljic.core.member;
+
+public class Grade {
+
+}
---
+++ server/src/main/java/nostaljic/core/member/MemberService.java
@@ -0,0 +1,6 @@
+package nostaljic.core.member;
+
+public interface MemberService {
+    void join(Member member);
+    Member findMember(Long memberId);
+}
