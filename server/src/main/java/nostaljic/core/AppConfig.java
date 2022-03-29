d2635f7cab43fdfa35df5153ca3c497fecb6c4a1
Scorpion <scorpion@dgu.ac.kr>
2022-03-30 00:35:46 +0900

    refactor: DIP, OCP

--- server/src/main/java/nostaljic/core/AppConfig.java
+++ server/src/main/java/nostaljic/core/AppConfig.java
@@ -1,6 +1,8 @@
 package nostaljic.core;
 
+import nostaljic.core.discount.DiscountPolicy;
 import nostaljic.core.discount.RateDiscountPolicy;
+import nostaljic.core.member.MemberRepository;
 import nostaljic.core.member.MemberService;
 import nostaljic.core.member.MemberServiceImpl;
 import nostaljic.core.member.MemoryMemberRepository;
@@ -8,11 +10,18 @@ import nostaljic.core.order.OrderService;
 import nostaljic.core.order.OrderServiceImpl;
 
 public class AppConfig {
-    //AppConfig를 통해서 의존성을 주입한다;
+    //역할만 추출한다.만약 추후에 Impl이 달라진다면 해당 부분만 변경하면 된다.
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    private DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+    //DIP : 의존성을 주입한다.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
     public OrderService orderService(){
-        return new OrderServiceImpl(new RateDiscountPolicy(),new MemoryMemberRepository());
+        return new OrderServiceImpl(discountPolicy(),memberRepository());
     }
 }
