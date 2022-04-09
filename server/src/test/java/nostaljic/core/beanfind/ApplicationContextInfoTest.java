adce4df1afd0ca4c5a4cf4bba51147dff6cd6f5b
nostaljic <scorpion@dgu.ac.kr>
2022-04-09 13:52:42 +0900

    test: search for beans

---
+++ server/src/test/java/nostaljic/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,18 @@
+package nostaljic.core.beanfind;
+import nostaljic.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Test
+    @DisplayName("Print Bean")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanName : beanDefinitionNames){
+            Object bean = ac.getBean(beanName);
+            System.out.println("ApplicationContextInfoTest.findAllBean "+ bean);
+        }
+    }
+}
